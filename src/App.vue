41f9af79ecfd93e20a0fc9614410539d604b71f6
mansmahamat <mansour.mahamat.salle@gmail.com>
2020-10-04 01:14:17 +0200

    add dummy list of resource

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <StoredResources :resources="storedResources"/>
+</template>
+
+<script>
+import StoredResources from './components/learning-resource/StoredResources'
+
+export default {
+    components : {
+        StoredResources
+    },
+    data() {
+        return {
+            storedResources : [
+                {
+                    id : 'documentation',
+                    title: 'Documentation',
+                    description : 'La documentation officielle de Vue.js',
+                    link : 'https://vuejs.org/'
+                },
+                {
+                    id : 'google',
+                    title: 'Google',
+                    description : 'Demande à Google...',
+                    link : 'https://www.google.fr/'
+                }
+            ]
+        }
+    },
+}
+</script>
+
+<style >
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/learning-resource/LearningResource.vue
@@ -0,0 +1,57 @@
+<template>
+    <li>
+        <div>
+            <header>
+                <h3> {{ title }} </h3>
+                <button>Supprimer</button>
+            </header>
+        </div>
+        <p> {{ description }} </p>
+        <nav>
+            <a target="_blank" :href="link">Voir la ressource</a>
+        </nav>
+    </li>
+</template>
+
+<script>
+export default {
+    props : [
+        'title',
+        'description',
+        'link'
+    ]
+}
+</script>
+
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #42b983;
+  font-weight: bolder;
+}
+
+a:hover,
+a:active {
+  color: teal;
+}
+</style>
